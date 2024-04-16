4fd1f5104b89747a85cedf3a99495af8fe9888f1
ben <ben.f@iterateagency.com>
2024-04-16 18:22:04 +1000

    editing footer message format

--- footer.php
+++ footer.php
@@ -70,27 +70,32 @@
                             </p>
 
                             <div class="">
-                                <form action="https://byablo.us8.list-manage.com/subscribe/post?u=966a983115dc44d6768748ad7&amp;id=711248dc13&amp;f_id=007403e0f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate d-flex" target="_blank" novalidate>
-                                    
-                                    <input type="email" value="" placeholder="Email Address" name="EMAIL" class="hof-footer__lower--input required email text-center text-lg-left mb-3" id="mce-EMAIL" required>
-                                    
-                                    <span id="mce-EMAIL-HELPERTEXT" class="hof-footer__lower--text helper_text"></span>
-                                    <div id="mce-responses" class="clear foot">
-                                        <div class="response hof-footer__lower--text" id="mce-error-response" style="display:none"></div>
-                                        <div class="response hof-footer__lower--text" id="mce-success-response" style="display:none"></div>
-                                    </div> 
-                                    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_966a983115dc44d6768748ad7_711248dc13" tabindex="-1" value=""></div>
-                                    
-                                    <div class="optionalParent">
-                                        <div class="clear foot hof-footer__lower--btn-container">
-                                            <input type="submit" value="" name="subscribe" id="mc-embedded-subscribe" class="hof-footer__lower--btn">
-                                            <div class="hof-footer__lower--btn-icon">
-                                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                    <path d="M12.7111 17.7099L17.7111 12.7099C17.8021 12.6148 17.8735 12.5027 17.9211 12.3799C18.0211 12.1365 18.0211 11.8634 17.9211 11.6199C17.8735 11.4972 17.8021 11.385 17.7111 11.2899L12.7111 6.28994C12.6178 6.1967 12.5072 6.12274 12.3853 6.07228C12.2635 6.02182 12.1329 5.99585 12.0011 5.99585C11.7348 5.99585 11.4794 6.10164 11.2911 6.28994C11.1028 6.47824 10.997 6.73364 10.997 6.99994C10.997 7.26624 11.1028 7.52164 11.2911 7.70994L14.5911 10.9999L7.00108 10.9999C6.73586 10.9999 6.48151 11.1053 6.29397 11.2928C6.10644 11.4804 6.00108 11.7347 6.00108 11.9999C6.00108 12.2652 6.10644 12.5195 6.29397 12.707C6.48151 12.8946 6.73586 12.9999 7.00108 12.9999L14.5911 12.9999L11.2911 16.2899C11.1974 16.3829 11.123 16.4935 11.0722 16.6154C11.0214 16.7372 10.9953 16.8679 10.9953 16.9999C10.9953 17.132 11.0214 17.2627 11.0722 17.3845C11.123 17.5064 11.1974 17.617 11.2911 17.7099C11.384 17.8037 11.4946 17.8781 11.6165 17.9288C11.7384 17.9796 11.8691 18.0057 12.0011 18.0057C12.1331 18.0057 12.2638 17.9796 12.3857 17.9288C12.5075 17.8781 12.6181 17.8037 12.7111 17.7099Z" fill="#372120"/>
-                                                </svg>
+                                <form action="https://byablo.us8.list-manage.com/subscribe/post?u=966a983115dc44d6768748ad7&amp;id=711248dc13&amp;f_id=007403e0f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                                    <div class="d-flex">
+                                        <input type="email" value="" placeholder="Email Address" name="EMAIL" class="hof-footer__lower--input required email text-center text-lg-left mb-3" id="mce-EMAIL" required>
+                                        
+                                        <span id="mce-EMAIL-HELPERTEXT" class="hof-footer__lower--text helper_text"></span>
+                                        <!-- <div id="mce-responses" class="clear foot">
+                                            <div class="response hof-footer__lower--text" id="mce-error-response" style="display:none"></div>
+                                            <div class="response hof-footer__lower--text" id="mce-success-response" style="display:none"></div>
+                                        </div>  -->
+                                        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_966a983115dc44d6768748ad7_711248dc13" tabindex="-1" value=""></div>
+                                        
+                                        <div class="optionalParent">
+                                            <div class="clear foot hof-footer__lower--btn-container">
+                                                <input type="submit" value="" name="subscribe" id="mc-embedded-subscribe" class="hof-footer__lower--btn">
+                                                <div class="hof-footer__lower--btn-icon">
+                                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                        <path d="M12.7111 17.7099L17.7111 12.7099C17.8021 12.6148 17.8735 12.5027 17.9211 12.3799C18.0211 12.1365 18.0211 11.8634 17.9211 11.6199C17.8735 11.4972 17.8021 11.385 17.7111 11.2899L12.7111 6.28994C12.6178 6.1967 12.5072 6.12274 12.3853 6.07228C12.2635 6.02182 12.1329 5.99585 12.0011 5.99585C11.7348 5.99585 11.4794 6.10164 11.2911 6.28994C11.1028 6.47824 10.997 6.73364 10.997 6.99994C10.997 7.26624 11.1028 7.52164 11.2911 7.70994L14.5911 10.9999L7.00108 10.9999C6.73586 10.9999 6.48151 11.1053 6.29397 11.2928C6.10644 11.4804 6.00108 11.7347 6.00108 11.9999C6.00108 12.2652 6.10644 12.5195 6.29397 12.707C6.48151 12.8946 6.73586 12.9999 7.00108 12.9999L14.5911 12.9999L11.2911 16.2899C11.1974 16.3829 11.123 16.4935 11.0722 16.6154C11.0214 16.7372 10.9953 16.8679 10.9953 16.9999C10.9953 17.132 11.0214 17.2627 11.0722 17.3845C11.123 17.5064 11.1974 17.617 11.2911 17.7099C11.384 17.8037 11.4946 17.8781 11.6165 17.9288C11.7384 17.9796 11.8691 18.0057 12.0011 18.0057C12.1331 18.0057 12.2638 17.9796 12.3857 17.9288C12.5075 17.8781 12.6181 17.8037 12.7111 17.7099Z" fill="#372120"/>
+                                                    </svg>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
+                                    <div id="mce-responses" class="clear foot text-center text-lg-left" style="max-width:266px">
+                                        <div class="response hof-footer__lower--text" id="mce-error-response" style="display:none"></div>
+                                        <div class="response hof-footer__lower--text" id="mce-success-response" style="display:none"></div>
+                                    </div> 
 
                                 </form>
                                 <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';fnames[5]='BIRTHDAY';ftypes[5]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
